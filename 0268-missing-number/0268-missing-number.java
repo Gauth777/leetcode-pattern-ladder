913851acacdf6adeef7cf3191aea721f0b17ec2b
Naman <Allenbarr2609@gmail.com>
2026-09-06 10:13:16 +0530

    Time: 0 ms (100%), Space: 47 MB (95.49%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,15 +1,13 @@
 class Solution {
     public int missingNumber(int[] nums) {
         int n = nums.length;
-        for ( int i = 0; i <=n; i++) {
-            boolean found = false;
-            for(int j = 0; j< n; j++) {
-                if(nums[j]==i) {
-                    found = true;
-                    break;
-                }
-            }
-            if(!found) {
+        boolean[] seen = new boolean[n+1];
+        for( int num:nums) {
+            seen[num] = true;
+
+        }
+        for(int i = 0; i<= n; i++) {
+            if(!seen[i]){
                 return i;
             }
         }
